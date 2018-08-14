88f1975b0234e7f91a7a021f9aee660b99fa7b79
James Rakich <james@fullandbydesign.com.au>
2018-08-14 21:47:23 +1000

    Day 25 - Intro to CSS using Weekends Web App promotion page.

--- build-tasks/styles.js
+++ build-tasks/styles.js
@@ -7,6 +7,7 @@ var CONFIG = require('./config')
 gulp.task("styles", function () {
   return gulp.src([
                CONFIG.paths.clientSrc + "/app.less",
+               CONFIG.paths.clientSrc + "/wwa_home.less",
                './node_modules/mocha/mocha.css',
              ])
              .pipe(less())
